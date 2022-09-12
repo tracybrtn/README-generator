6cdacb7905b27629a9b80de5b81efe4e77dd566e
tracybrtn <tracynburton@gmail.com>
2022-09-11 19:54:02 -0500

    working inquirer

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Please describe your project (required)'
+        },
+        {
+            type: 'input',
+            name: 'Installation instructions',
+            message: 'Please enter instructions for installation (required)'
+        },
+        {
+            type: 'input',
+            name: 'Usage Information',
+            message: 'Please explain usage of your application (required)'
+        },
+        {
+            type: 'input',
+            name: 'Contribution guidelines',
+            message: 'Please describe how users can contribute to your application (required)'
+        },
+        {
+            type: 'input',
+            name: 'Test Instructions',
+            message: 'Please enter instructions for user testing (required)'
+        }
+        //add validation to all input
+    ]);
+}
+
+promptUser();
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//writeToFile();
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+// init();
